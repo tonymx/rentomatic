ed6734c0b9de70c17e803fba7b1de7fa2f43e3d5
Tony Diaz <info@tonydiaz.pro>
2024-05-17 23:32:55 -0600

    finishing chapter 04

--- tests/domain/test_room.py
+++ tests/domain/test_room.py
@@ -1,5 +1,10 @@
 import uuid
+import json
+
+from rentomatic.serializers.room import RoomJsonEncoder
 from rentomatic.domain.room import Room
+
+
 def test_room_model_init():
     code = uuid.uuid4()
 
@@ -57,3 +62,26 @@ def test_room_model_comparation():
     room2= Room.from_dict(init_dict)
 
     assert room1==room2
+
+def test_serialize_domain_room():
+    code = uuid.uuid4()
+    room = Room(
+               code=code,
+               size=200,
+               price=10,
+               longitude=-0.09998975,
+               latitude=51.75436293,
+            )
+    expected_json = f"""
+                        {{
+                        "code": "{code}",
+                        "size": 200,
+                        "price": 10,
+                        "longitude": -0.09998975,
+                        "latitude": 51.75436293
+                        }}
+                        """
+    json_room = json.dumps(room, cls=RoomJsonEncoder)
+    #json_room = json.dumps(room)
+    print(json_room)
+    assert json.loads(json_room) == json.loads(expected_json)
